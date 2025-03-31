93fe2297bf2dd3617610a59102bbd1a54bf5dec3
urbanmeeple <bergejoel@gmail.com>
2025-03-31 14:44:11 +0200

    add vibe game jam link

--- index.php
+++ index.php
@@ -14,5 +14,6 @@
   </div>
   <!-- Removed direct inclusion of js/config.js.php since it is imported via main.js as a module -->
   <script type="module" src="js/main.js"></script>
+  <a target="_blank" href="https://jam.pieter.com" style="font-family: 'system-ui', sans-serif; position: fixed; bottom: -1px; right: -1px; padding: 7px; font-size: 14px; font-weight: bold; background: #fff; color: #000; text-decoration: none; z-index: 10; border-top-left-radius: 12px; z-index: 10000; border: 1px solid #fff;">🕹️ Vibe Jam 2025</a>
 </body>
 </html>
